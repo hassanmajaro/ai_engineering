b56759e1eb8f6a8ce8efa4ece8f764beff70a6a9
Hassan Majaro <hhassanzino75@gmail.com>
2025-10-17 15:05:08 +0100

    today's task

--- module_3/week11/backend_tut/app.py
+++ module_3/week11/backend_tut/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 import uvicorn
 import os
@@ -10,12 +10,35 @@ import os
 load_dotenv()
 
 app = FastAPI(title = "Simple FastAPI App", version = "1.0.0")
+data = [{"name": "Sam Larry", "age": 20, "track": "AI Developer"},
+        {"name": "Bahuballi", "age":21, "track": "Backend Developer"},
+        {"name": "John Doe", "age": 22, "track": "Ethical Hacker"}]
 
-@app.get("/")
+class Item(BaseModel):
+    name: str = Field(..., example = "Perpetual")
+    age: int = Field(..., example = 25)
+    track: str = Field(..., example = "Fullstack Developer")
+
+@app.get("/", description = "This endpoint just returns a welcome message")
 def root():
-    return {"Message": "Welcome to my fastAPI Application"}
+    return {"Message": "Welcome to my FastAPI Application"}
 
-if __name__ == "  main  ":
+@app.get("/get data")
+def get_data():
+    return data 
+
+@app.post("/create data")
+def create_data(req: Item):
+    data.append(req.dict())
+    return {"Message": "Data Received", "Data": data}
+
+@app.put("/update-data/{id}")
+def update_data(id: int, req: Item):
+    data[id] - req.dict()
+    print(data)
+    return {"Message": "Data Updated", "Data": data}
+
+if __name__ == "__main__":
     print(os.getenv("host"))
     print(os.getenv("port"))
-    uvicorn.run(app, host = os.getenv("host"), port = os.getenv("port"))
+    uvicorn.run(app, host = os.getenv("host"), port = int(os.getenv("port")))
